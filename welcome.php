d59cf883eed4779f897af7b75059098b17429074
csxavier <60736406+csxavier@users.noreply.github.com>
2020-06-06 15:51:46 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		Welcome <?php 
+			$gender = $_POST["Gender"]
+		echo $Gender
+		?>
+		<br>
+		Your email address is: <?php echo $_POST["activity"]; ?>
+</body>
+</html>
